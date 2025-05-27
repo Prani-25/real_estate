729c574188ee7a0b3b4d453df424d5562a7b1a12
Praneeth <praneethummadisetty@gmail.com>
2025-05-28 00:04:45 +0530

    Update signup.jsx

--- CLIENT/src/pages/signup.jsx
+++ CLIENT/src/pages/signup.jsx
@@ -296,7 +296,7 @@ function Signup() {
 
     // Submit data to backend
     try {
-      const res = await fetch("http://localhost:5000/api/signup", {
+      const res = await fetch("https://real-estate-zmcq.onrender.com/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
